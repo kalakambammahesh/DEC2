1840c72c31f0fde3096b17b6db12f0064b7ee8c9
kalakambammahesh <33896435+kalakambammahesh@users.noreply.github.com>
2021-07-14 08:19:49 +0530

    Delete MainMethodOverridingAndOverLoading.java

--- MainMethodOverridingAndOverLoading.java
+++
@@ -1,9 +0,0 @@
-package com.pre;
-
- class Test  {
-	public static void main(String[] args) {
-		System.out.println("Parent main....");
-	}
-}
-
- public class MainMethodOverridingAndOverLoading extends Test{}
